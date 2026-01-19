3f8bee47650821dba0bd118192d7b7e453ec41d1
Muhammad Rizqi Asshidiiq <mrizqiassh18@gmail.com>
2026-01-19 10:48:56 +0700

    fourth commit

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -14,6 +14,7 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
+  NavigationMenuViewport,
 } from '@/components/ui/navigation-menu';
 
 export default function Header() {
@@ -22,24 +23,24 @@ export default function Header() {
       <Link href="/">
         <img src="/logo/logo.png" alt="logo" className="w-20" />
       </Link>
-      <nav className="flex-column flex items-center text-base font-semibold text-white">
-        <div className="hidden md:flex">
-          <NavigationMenu>
-            <NavigationMenuList>
+      <nav>
+        <div className="hidden h-full items-center md:flex">
+          <NavigationMenu className="relative">
+            <NavigationMenuList className="flex gap-6">
               <NavigationMenuItem>
                 <NavigationMenuTrigger className="bg-transparent text-white hover:bg-transparent hover:text-white focus:bg-transparent focus:text-white focus-visible:bg-transparent focus-visible:text-white data-[state=open]:bg-transparent data-[state=open]:text-white [&>svg]:text-white">
                   Home
                 </NavigationMenuTrigger>
 
-                <NavigationMenuContent className="bg-black/80 backdrop-blur">
+                <NavigationMenuContent className="bg-gray-100 backdrop-blur">
                   <NavigationMenuLink asChild>
-                    <Link href="/" className="block px-4 py-2 text-white hover:bg-white/10">
+                    <Link href="/" className="block px-4 py-2 hover:bg-white/10">
                       Home
                     </Link>
                   </NavigationMenuLink>
 
                   <NavigationMenuLink asChild>
-                    <Link href="/help" className="block px-4 py-2 text-white hover:bg-white/10">
+                    <Link href="/help" className="block px-4 py-2 hover:bg-white/10">
                       Help
                     </Link>
                   </NavigationMenuLink>
@@ -50,32 +51,29 @@ export default function Header() {
                   Auth
                 </NavigationMenuTrigger>
 
-                <NavigationMenuContent className="bg-black/80 backdrop-blur">
+                <NavigationMenuContent className="bg-gray-100 backdrop-blur">
                   <NavigationMenuLink asChild>
-                    <Link
-                      href="/auth/login"
-                      className="block px-4 py-2 text-white hover:bg-white/10"
-                    >
+                    <Link href="/auth/login" className="block px-4 py-2 hover:bg-white/10">
                       Login
                     </Link>
                   </NavigationMenuLink>
 
                   <NavigationMenuLink asChild>
-                    <Link
-                      href="/auth/register"
-                      className="block px-4 py-2 text-white hover:bg-white/10"
-                    >
+                    <Link href="/auth/register" className="block px-4 py-2 hover:bg-white/10">
                       Register
                     </Link>
                   </NavigationMenuLink>
                 </NavigationMenuContent>
               </NavigationMenuItem>
             </NavigationMenuList>
+            <NavigationMenuViewport className="bg-black/80 backdrop-blur" />
           </NavigationMenu>
         </div>
         <div className="md:hidden">
           <DropdownMenu>
-            <DropdownMenuTrigger className="bg-transparent text-4xl">≡</DropdownMenuTrigger>
+            <DropdownMenuTrigger className="bg-transparent text-4xl text-white">
+              ≡
+            </DropdownMenuTrigger>
             <DropdownMenuContent>
               <DropdownMenuGroup>
                 <DropdownMenuItem>
